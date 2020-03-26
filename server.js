c82316fa5777501e96d9fad14bce4f84b112b998
Manda <mandakadam@gmail.com>
2020-03-26 10:30:30 +0530

    bug fixes while build

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-// server.js
-const { createServer } = require('http')
-const { parse } = require('url')
+const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+
+console.log('Env:', process.env.NODE_ENV);
+
+const app = next({
+    dir: '.',
+    dev
+ })
+
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+app.prepare()
+    .then(() => {
+        const server = express()
 
-    if (pathname === '/a') {
-      app.render(req, res, '/b', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/a', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(3000, err => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+        server.set('port', (process.env.PORT || 3000));
+
+        server.get('/user/:userId', (req, res) => {
+            return app.render(req, res, '/agent', req.query)
+        })
+
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
+
+        server.listen(server.get('port'), (err) => {
+            if (err) throw err
+            console.log(`> Ready on http://localhost:${server.get('port')}`)
+        })
+    })
